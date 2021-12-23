49dcdd52874451f6e698070c188c31f74aebde32
Nevin Edwin <nevin.e@inapp.com>
2021-12-23 10:47:25 +0530

    initial commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className='nav-cont'>
+            <h2 className='nav-title'>Meme Generator</h2>
+            <h5 className='nav-des'>React Course - Project 3</h5>
+        </div>
+    )
+}
+
+export default Navbar
